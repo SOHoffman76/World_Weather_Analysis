7ca011fa8f9e74755bb1a71ddfbc04947fca70b9
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 <Shannon>
2024-05-05 21:17:06 -0500

    Saving homework progress

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7c3488d69ac2f4ada5c8706e16dc5664"
+weather_api_key = "ba83a2d34dc19d2fec5f3ed173f4329e"
 
 # Geoapify API Key
 geoapify_key = "d2ad28a6ba4d4df59d77a9be61d9f8bd"
